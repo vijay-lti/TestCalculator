385b02dccc26e87fa60276aaff88b5b54f4e6761
vijayakumar.rajendra <vijayakumar.rajendran@olamnet.com>
2020-01-04 17:23:08 +0530

    updated display method

--- src/com/test/calc/CalcFunctions.java
+++ src/com/test/calc/CalcFunctions.java
@@ -1,9 +1,12 @@
 package com.test.calc;
 
 public class CalcFunctions {
-	private int data[] = {0, 4, 42};
+	private int data[] = {-1, 0, 4, 42, 84};
 
 	public void display() {
 		System.out.println("TODO: Implement data display");
+		for(int i:data) {
+			System.out.println("Next Value: "+data[i]);
+		}
 	}
 }
