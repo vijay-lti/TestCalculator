7174f92aad9760daa9cd9c1660f040a591fb01a1
vijayakumar.rajendra <vijayakumar.rajendran@olamnet.com>
2020-01-04 17:32:49 +0530

    summed values and added comments

--- src/com/test/calc/CalcFunctions.java
+++ src/com/test/calc/CalcFunctions.java
@@ -1,9 +1,17 @@
 package com.test.calc;
-
+/*
+ * comments to be added
+ */
 public class CalcFunctions {
 	private int data[] = {0, 4, 42};
 
 	public void display() {
 		System.out.println("TODO: Implement data display");
+		int sum=0;
+		for (int i=0;i<data.length;i++) {
+			System.out.println("Next Values: "+data[i]);
+			sum +=i;
+		}
+		System.out.println("Sum is: "+sum);
 	}
 }
